c6cbbb0cff6d4881307d007a2d927d1aa636bfd2
lumiplum <whiteplum20060329@gmail.com>
2026-02-09 17:48:17 -0500

    homework cat move part

---
+++ Assets/homework script/cat move.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class catmove : MonoBehaviour
+{
+    public float speed;
+
+    private float direction = 1f;
+
+   private float gravity = -0.2f;
+    // Gravity strength, controls how strongly the object is pulled downward each frame
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.aKey.isPressed)
+        {
+            transform.position += -1 * direction * transform.right * speed * Time.deltaTime;
+        }
+        if (Keyboard.current.dKey.isPressed)
+        {
+            transform.position += direction * transform.right * speed * Time.deltaTime;
+        }
+        if (Keyboard.current.wKey.isPressed)
+        {
+            transform.position += direction * transform.up * speed * Time.deltaTime;
+          
+
+        }
+        transform.position += new Vector3(0, gravity, 0) * Time.deltaTime;//I used a  variable to simulate gravity.
+       // Gravity reduces every frame, creating a natural motion where the object  falls.
+
+    }
+}
